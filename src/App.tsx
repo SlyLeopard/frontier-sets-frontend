6309af615746c28dd24caa21d402c24f8a2fdeb5
Alex <aaahandour@gmail.com>
2026-03-10 00:07:11 -0400

    implement browser router

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PokemonSetSearchPage from './pages/PokemonSetSearchPage'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Routes>
+      <Route path="/" element={<>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +31,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more about deez nuts
       </p>
-    </>
+    </>} />
+      <Route path="/pokemonSets" element={<PokemonSetSearchPage />} />
+    </Routes>
   )
 }
 
