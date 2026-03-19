b7ff9818365035c8e756283982e9591b04500099
Alex <aaahandour@gmail.com>
2026-03-19 00:40:52 -0400

    add raw set stats to table

--- src/components/PokemonTable.tsx
+++ src/components/PokemonTable.tsx
@@ -20,20 +20,29 @@ export default function PokemonTable({ sets }: Props) {
           <TableCell>Name</TableCell>
           <TableCell>Species</TableCell>
           <TableCell>Item</TableCell>
-          <TableCell>Nature</TableCell>
-          
-          
+          <TableCell>HP</TableCell>
+          <TableCell>Atk</TableCell>
+          <TableCell>Def</TableCell>
+          <TableCell>SpA</TableCell>
+          <TableCell>SpD</TableCell>
+          <TableCell>SpE</TableCell>
         </TableRow>
       </TableHead>
 
       <TableBody>
-        {sets.map((pokemon) => (
-          <TableRow key={pokemon.name}>
-            <TableCell><img src={pokemon.iconUrl} width="50"></img></TableCell>
-            <TableCell>{pokemon.name}</TableCell>
-            <TableCell>{pokemon.species}</TableCell>
-            <TableCell>{pokemon.item}</TableCell>
-            <TableCell>{pokemon.nature}</TableCell>
+        {sets.map((set) => (
+          <TableRow key={set.name}>
+            <TableCell><img src={set.iconUrl} width="50"></img></TableCell>
+            <TableCell>{set.name}</TableCell>
+            <TableCell>{set.species}</TableCell>
+            <TableCell>{set.item}</TableCell>
+            <TableCell>{set.nature}</TableCell>
+            <TableCell>{set.pokemon.stats[0]}</TableCell>
+            <TableCell>{set.pokemon.stats[1]}</TableCell>
+            <TableCell>{set.pokemon.stats[2]}</TableCell>
+            <TableCell>{set.pokemon.stats[3]}</TableCell>
+            <TableCell>{set.pokemon.stats[4]}</TableCell>
+            <TableCell>{set.pokemon.stats[5]}</TableCell>
           </TableRow>
         ))}
       </TableBody>
